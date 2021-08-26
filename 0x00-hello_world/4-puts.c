333c6e4be0b09f7c859df6eb83dd8059be561368
Mikiyas Eyob <86609715+realmikiyas@users.noreply.github.com>
2021-08-26 05:45:19 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 /**
- * main - Entry point
+ * main - This is the entry point of the project
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
