6a8d8a782dc1e2602caec9dc48c23712ddae2930
Mikiyas Eyob <86609715+realmikiyas@users.noreply.github.com>
2021-08-31 06:39:55 -0700

    all

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- *main = prints holberton as message.
+ * main - Prints Holberton as a message.
  *
- *return (0) always success
+ * Return: Always 0 (Success)
  */
-
-int main void()
+int main(void)
 {
-  int str[]={72
+int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+int count, sz;
+
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);	}
+_putchar('\n');
+return (0);
+}
