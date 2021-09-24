d6ce2ed1d03ba6973f5b6039f1586bb3eee1874a
Mikiyas Eyob <mikiyase744@gmail.com>
2021-09-24 04:32:17 -0700

    update more maloce

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,28 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* array_range - creates an array of integers.
-* @min: minimum value.
-* @max: maximum value.
-*
-* Return: pointer to the newly created array.
-* if man > mix, returns NULL.
-* if malloc fails, returns NULL.
-*/
+ * array_range - creates an array of integers
+ * @min: minimum integer
+ * @max: maximum integer
+ * Return: pointer to allocated memory
+ */
+
 int *array_range(int min, int max)
 {
-int *ar;
-int i;
+	int *arr, diff, num, j;
 
-if (min > max)
-return (NULL);
+	if (min > max)
+		return (NULL);
+	diff = max - min + 1;
+	arr = malloc(diff * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 
-ar = malloc(sizeof(*ar) * ((max - min) + 1));
+	for (num = min, j = 0; num <= max && j < diff; num++, j++)
+		arr[j] = num;
 
-if (ar == NULL)
-return (NULL);
-
-for (i = 0; min <= max; i++, min++)
-ar[i] = min;
-
-return (ar
+	return (arr);
+}
