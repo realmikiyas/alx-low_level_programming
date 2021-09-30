6a36db72917e383dd2dd6915632f7b37674b081f
Mikiyas Eyob <mikiyase744@gmail.com>
2021-09-30 12:58:14 -0700

    2nd update to vardiac fun

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,51 +1,47 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
-* print_all - prints anything.
-* @format: a list of types of arguments passed to the function.
-*
-* Return: no return.
-*/
+ * print_all - prints anything
+ * @format: format of data type to be printed
+ */
 void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
+	unsigned int i = 0;
+	char *str, *sep = "";
+	va_list arguments;
 
-va_start(valist, format);
-while (format && format[i])
-{
-j = 0;
-while (t_arg[j])
-{
-if (format[i] == t_arg[j] && c)
-{
-printf(", ");
-break;
-} j++;
-}
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
-break;
-case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
-break;
-case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
-break;
-case 's':
-str = va_arg(valist, char *), c = 1;
-if (!str)
-{
-printf("(nil)");
-break;
-}
-printf("%s", str);
-break;
-} i++;
-}
-printf("\n"), va_end(valist);
+	va_start(arguments, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(arguments, int));
+					break;
+				case 'i':
+					printf("%s%i", sep, va_arg(arguments, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(arguments, double));
+					break;
+				case 's':
+					str = va_arg(arguments, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(arguments);
 }
